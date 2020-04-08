f187a6753d0e5732dcb19fc3f303158e27db2125
Kfir <kfirg15@gmail.com>
2020-04-08 15:46:08 +0300

    Eployees + data fileds +Main

---
+++ Employees.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_4
+{
+    class Employees
+    {
+        public enum Days { Sunday = 1, Monday, Tuesday, Wednesday, Thursday, Friday, Satuerday }
+
+        //data fields:
+        string name;
+        int age;
+        int id;
+        Days day;
+
+        public static int workersCounter;
+
+    }
+}
